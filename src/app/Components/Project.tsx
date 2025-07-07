8a54b7db8dd2d345ec5c7b2f41f3b80ed766b8d9
Namalekanayaka <namalekanayake412@gmail.com>
2025-07-07 10:37:20 +0530

    update-project-cards

--- src/app/Components/Project.tsx
+++ src/app/Components/Project.tsx
@@ -5,19 +5,21 @@ const Project = () => {
       description: "This is a brief description of Project One.",
       imageUrl: "/project1.jpg",
       link: "#",
-      tags: ["React", "JavaScript", "Tailwind CSS"],
+      tags: ["React", "JavaScript", "TailwindCSS"],
     },
     {
       title: "Project Two",
       description: "This is a brief description of Project Two.",
       imageUrl: "/project2.jpg",
       link: "#",
+      tags: ["React", "JavaScript", "TailwindCSS"],
     },
     {
       title: "Project Three",
       description: "This is a brief description of Project Three.",
       imageUrl: "/project3.jpg",
       link: "#",
+      tags: ["React", "JavaScript", "TailwindCSS"],
     },
   ];
   return (
@@ -31,9 +33,27 @@ const Project = () => {
             key={index}
             className="bg-gray-800/30 backdrop-blur-sm rounded-lg p-6 border border-gray-700/50 hover:border-purple-500 transition-colors hoevr:shadow-sm"
           >
-            <div>
-              
-            {project.title}
+            <div className="flex flex-col h-full">
+              <h3 className="text-lg font-semibold text-white">
+                {project.title}
+                <div>
+                  {project.tags.map((tag, Index) => (
+                    <span
+                      key={Index}
+                      className="px-2 py-0.5 text-xs rounded-full bg-purple-500/20 text-purple-200 border border-purple-500/30 "
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <br />
+
+                <br />
+
+                {project.description}
+                <br />
+              </h3>
+            </div>
           </div>
         ))}
       </div>
